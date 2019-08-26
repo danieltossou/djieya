fde84b04de1e3e79127330b63a306674875b90c0
Hasseye <oumarm611@gmail.com>
2019-08-26 10:14:34 +0000

    Reglage de l'inscription d'etudiant

--- app/controllers/etudiants_controller.rb
+++ app/controllers/etudiants_controller.rb
@@ -71,7 +71,16 @@ class EtudiantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def etudiant_params
-      params.require(:etudiant).permit(:nom, :prenom, :sexe, :date_naissance, :matricule, :num_inscription, :classe_room_id, :oriente, :nationnalite, :email, :adresse, :contact, :maladie, :nom_parent, :contact_parent, :lien_parente, :email_parent, :photo, :autre, :ecole_id, :user_id, dossier_ids: [])
+    # Generation d'un numero d'inscription automatique
+      @last_etudiant = Etudiant.last.id
+      @id = @last_etudiant + 1
+      @date = Time.now.strftime("%Y").to_s
+      @nom = params[:etudiant][:nom][0].to_s
+      @prenom = params[:etudiant][:prenom][0].to_s
+      @num_inscr = @date+'-'+@id.to_s+'-'+@nom+''+@prenom
+      params[:etudiant][:num_inscription] = @num_inscr
+
+      params.require(:etudiant).permit(:nom, :prenom, :sexe, :date_naissance, :montant, :matricule, :num_inscription, :classe_room_id, :oriente, :nationnalite, :email, :adresse, :contact, :maladie, :nom_parent, :contact_parent, :lien_parente, :email_parent, :photo, :autre, :ecole_id, :user_id, dossier_ids: [])
     end
 
 
--- app/models/etudiant.rb
+++ app/models/etudiant.rb
@@ -7,17 +7,7 @@ class Etudiant < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   after_create do
-
     @etudiant = Etudiant.last
-    # Generation d'un numero d'inscription automatique
-    @id = @etudiant.id.to_s
-    @date = Time.now.strftime("%Y").to_s
-    @nom = @etudiant.nom[0].to_s
-    @prenom = @etudiant.prenom[0].to_s
-    @num_inscr = @date+'-'+@id+'-'+@nom+''+@prenom
-    @etudiant.update(num_inscription: @num_inscr)
-
-    #
     @montant = @etudiant.classe_room.montant
     @annee = 1
     Inscription.create(etudiant_id: @etudiant.id, user_id: @etudiant.user_id, annee_id: @annee, classe_room_id: @etudiant.classe_room_id, ecole_id: @etudiant.ecole_id, montant: @montant)
