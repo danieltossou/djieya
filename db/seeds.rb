2145077faf30a04db2d7b313e4583dae0754e807
Hasseye <oumarm611@gmail.com>
2019-09-04 15:43:46 +0000

    A

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(email: 'admin@djieya.com', password: 'password', password_confirmation: 'password') if Rails.env.production?
+Admin.create!(email: 'admin@djieya.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
