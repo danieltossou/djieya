06e6cf42f29cd9c34423df8e526b0b001283b144
Hasseye <oumarm611@gmail.com>
2019-09-12 01:27:43 +0000

    Eleve de est_connecte? dans dossier_controllers

--- app/controllers/dossiers_controller.rb
+++ app/controllers/dossiers_controller.rb
@@ -1,7 +1,6 @@
 class DossiersController < ApplicationController
   before_action :set_dossier, only: [:show, :edit, :update, :destroy]
   #load_and_authorize_resource
-  skip_before_action :est_connecte?, only: :new
   
   # GET /dossiers
   # GET /dossiers.json
